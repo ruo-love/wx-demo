a9da03ae44c5c8080f2c115a97b18863f8fc7ee5
赵乾程 <zl18180276692@outlook.com>
2023-04-10 12:32:52 +0800

    second

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World1111',
+    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
